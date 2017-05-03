b3b0f03696567e98d4d447542991edeb80a80223
Alexandr Zakharov <sasha750i@mail.ru>
2017-05-03 18:41:47 +0300

    zip to gitignore

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import pandas as pd
 import utm
-import sys
+import os
 
 
 def main():
-    input_file = sys.argv[1]
-    if input_file.endswith('xlsx'):
-        for_id = input_file[0]
+    for dir_root, dirs, files in os.walk('Nodes'):
+        for file in files:
+            file_path = os.path.join(dir_root, file)
+            if file.endswith('xlsx'):
+                for_id = file[0]
 
-        def id_lon_lat(row):
-            new_id = for_id + '--' + unicode(int(row[0]))
-            lat, lon = utm.to_latlon(row[1], row[2], 18, 'N')
-            return new_id, lat, lon
+                def id_lon_lat(row):
+                    new_id = for_id + '--' + unicode(int(row[0]))
+                    lat, lon = utm.to_latlon(row[1], row[2], 18, 'N')
+                    return new_id, lat, lon
 
-        df = pd.read_excel(input_file, parse_cols='F,H,I', names=['stop_id', 'stop_lat', 'stop_lon'])
-        df = df.drop_duplicates(subset='stop_id')
-        df = df.apply(id_lon_lat, axis=1)
-        df = pd.DataFrame(df.tolist(), columns=['stop_id', 'stop_lat', 'stop_lon'], index=df.index)
+                df = pd.read_excel(file_path, parse_cols='F,H,I', names=['stop_id', 'stop_lat', 'stop_lon'])
+                df = df.drop_duplicates(subset='stop_id')
+                df = df.apply(id_lon_lat, axis=1)
+                df = pd.DataFrame(df.tolist(), columns=['stop_id', 'stop_lat', 'stop_lon'], index=df.index)
 
-        df.to_csv('from_excel.txt', index=False)
+                df.to_csv('from_excel.txt', index=False)
 
-    else:
-        Nodes = pd.read_csv(input_file, usecols=[0, 4, 5])
-        Nodes.to_csv('Nodes.csv', index=False)
+            elif file.endswith('.txt'):
+                Nodes = pd.read_csv(file_path, usecols=[0, 4, 5])
+                Nodes.to_csv('Nodes.csv', index=False)
 
 
 if __name__ == '__main__':
