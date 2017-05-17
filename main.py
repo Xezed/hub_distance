54d3c8a749217fcf3c984e43cbe1cc3aa5debe01
Alexandr Zakharov <sasha750i@mail.ru>
2017-05-17 12:42:08 +0300

    Comments and tweaks

--- main.py
+++ main.py
@@ -1,99 +1,100 @@
+# -*- coding: utf-8 -*-
 from math import radians, cos, sin, asin, sqrt
 import pandas as pd
 import multiprocessing
 import utm
-import sqlite3
 from contextlib import closing
 import os
 import sys
+import time
 import unicodecsv as csv
 from simpledbf import Dbf5
 
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
+columns_nodes_refstop = ['a_nodo', 'nodo_lat', 'nodo_lon', 'stop_id', 'stop_lat', 'stop_lon', 'stop_type', 'distance']
+columns_trunkstop_parent = ['stop_id', 'stop_lat', 'stop_lon', 'station_id', 'station_lat', 'station_lon', 'stop_type', 'distance']
+
+
 def main():
     try:
-        name = sys.argv[1]
+        input_stops = sys.argv[1]
     except:
-        name = False
+        input_stops = False
 
     nodes = pd.DataFrame()
 
     def id_lon_lat(row):
-        new_id = for_id + '--' + unicode(int(row[0]))
+        a_nodo = agency_id + '--' + unicode(int(row[0]))
         lat, lon = utm.to_latlon(row[1], row[2], 18, 'N')
-        return new_id, lat, lon
+        return a_nodo, lat, lon
 
-    if not name:
+    # create dataframes from distance matrices
+    if not input_stops:
         for dir_root, dirs, files in os.walk('Nodes'):
             for file in files:
                 file_path = os.path.join(dir_root, file)
                 print file_path
                 if file.endswith('xlsx'):
 
-                    for_id = file[0]
+                    agency_id = file[0]
 
-                    if file.startswith('Troncal'):
-                        df_troncal = pd.read_excel(file_path, parse_cols='G,P,Q', names=['stop_id', 'stop_lat', 'stop_lon'], index_col=False)
-                        df_troncal = df_troncal.drop_duplicates(subset='stop_id')
-                        df_troncal = df_troncal.apply(id_lon_lat, axis=1)
-                        df_troncal = pd.DataFrame(df_troncal.tolist(), columns=['stop_id_ref', 'stop_lat_ref', 'stop_lon_ref'], index=df_troncal.index)
-                        df_troncal['stop_type_ref'] = 'T'
+                    # create dataframe of trunk stops
+                    if agency_id == 'T':
+                        df = pd.read_excel(file_path, parse_cols='M,P,Q', names=['stop_id', 'stop_lat', 'stop_lon'])
+                        df = df.drop_duplicates(subset='stop_id')
+                        df = df.apply(id_lon_lat, axis=1)
+                        trunk_stops = pd.DataFrame(df.tolist(), columns=['stop_id', 'stop_lat', 'stop_lon'], index=df.index)
+                        trunk_stops['stop_type'] = 'T'
                         continue
 
-                    if file.startswith('Dual'):
-                        df = pd.read_excel(file_path, parse_cols='N,Q,R', names=['stop_id', 'stop_lat', 'stop_lon'])
+                    # create dataframe of non-trunk nodes
+                    elif agency_id == 'D':
+                        df = pd.read_excel(file_path, parse_cols='N,Q,R', names=['a_nodo', 'nodo_lat', 'nodo_lat'])
 
                     else:
-                        df = pd.read_excel(file_path, parse_cols='F,H,I', names=['stop_id', 'stop_lat', 'stop_lon'])
+                        df = pd.read_excel(file_path, parse_cols='F,H,I', names=['a_nodo', 'nodo_lat', 'nodo_lat'])
 
-                    df = df.drop_duplicates(subset='stop_id')
+                    df = df.drop_duplicates(subset='a_nodo')
                     df = df.apply(id_lon_lat, axis=1)
-                    df = pd.DataFrame(df.tolist(), columns=['stop_id', 'stop_lat', 'stop_lon'], index=df.index)
+                    df = pd.DataFrame(df.tolist(), columns=['a_nodo', 'nodo_lat', 'nodo_lat'], index=df.index)
                     nodes = nodes.append(df)
 
+    # create dataframe of nodes from stops.txt input
     else:
-        df = pd.read_csv(name, usecols=[0, 4, 5])
-        nodes = df
-    # nodes.to_csv('Nodes.csv', index=False)
+        df = pd.read_csv(input_stops, usecols=[0, 4, 5], names=['a_nodo', 'nodo_lat', 'nodo_lat'], index_col=False)
+        nodes = df[df.a_nodo.str[0] != 'T']
 
-    dbf = Dbf5('Ref Layers/Cenefas/Cenefas/Cenefas.dbf')
+        trunk_stops = df[df.a_nodo.str[0] == 'T']
+        trunk_stops = pd.DataFrame(trunk_stops.tolist(), columns=['stop_id', 'stop_lat', 'stop_lon'], index=trunk_stops.index)
+        trunk_stops['stop_type'] = 'T'
+
+    # collate road side ref stops from Cenefas input
+    dbf = Dbf5('Ref Layers/Cenefas/Cenefas.dbf')
     df = dbf.to_dataframe()
-    df.to_csv('cenefas.csv', columns=['CENEFA', 'Latitud', 'Longitud'], index=False)
-    ref_stops = pd.read_csv('cenefas.csv', header=0, names=['stop_id_ref', 'stop_lat_ref', 'stop_lon_ref'], index_col=False)
-    ref_stops['stop_type_ref'] = 'Z'
-    if not name:
-        ref_stops = ref_stops.append(df_troncal, ignore_index=True)
-    else:
-        df = pd.read_csv(name, usecols=[0, 4, 5], header=0, names=['stop_id_ref', 'stop_lat_ref', 'stop_lon_ref'], index_col=False)
-        df = df[df.stop_id_ref.str[0] == 'T']
-        df['stop_type_ref'] = 'T'
-        global ref_stops
-        ref_stops = ref_stops.append(df)
+    df.to_csv('cenefas.csv', columns=['CENEFA', 'NOMBRE', 'DIRECCION', 'LOCALIDAD', 'Longitud', 'Latitud'], encoding='utf-8', index=False)
+    
+    global ref_stops
+    ref_stops = pd.read_csv('cenefas.csv', header=0, usecols=[0, 4, 5], names=['stop_id', 'stop_lon', 'stop_lat'], index_col=False)
+    #ref_stops = pd.DataFrame(cenefas.tolist(), columns=['stop_id', 'stop_lat', 'stop_lon'])
+    ref_stops['stop_type'] = 'Z'
 
-    print len(nodes), len(ref_stops)
-
-    fields = ['stop_id', 'stop_lat', 'stop_lon', 'stop_id_ref', 'stop_lat_ref', 'stop_lon_ref', 'stop_type_ref',
-              'distance']
-
-    with open('NodesRefStop.csv', 'w') as f:
-        with open('UnmatchedNodes.csv', 'w') as un:
-            writer = csv.writer(f)
-            writer.writerow(fields)
-            writer = csv.writer(un)
-            writer.writerow(fields)
+    # append trunk stops from nodes
+    ref_stops = ref_stops.append(trunk_stops, ignore_index=True)
 
     with closing(multiprocessing.Pool()) as pool:
         # joined_rows will contain lists of joined rows in arbitrary order.
         # use name=None so we get proper tuples, pandas named tuples cannot be pickled, see https://github.com/pandas-dev/pandas/issues/11791
-        joined_rows = pool.imap_unordered(join_rows, nodes.itertuples(name=None))
+        joined_rows = pool.imap_unordered(join_rows_unify, nodes.itertuples(name=None))
 
         # open file and write out all rows from incoming lists of rows
-        with open('NodesRefStop.csv', 'a') as f:
-            with open('UnmatchedNodes.csv', 'a') as un:
+        with open('nodes_refstop.csv', 'a') as f:
+            with open('unmatched_nodes.csv', 'a') as un:
                 writer = csv.writer(f)
                 un_writer = csv.writer(un)
+                writer.writerow(columns_nodes_refstop)
+                un_writer.writerow(columns_nodes_refstop)
                 for row_list in joined_rows:
                     min_dist = sys.maxint
                     min_row = ''
@@ -106,29 +107,26 @@ def main():
                     else:
                         writer.writerow(min_row)
 
-    nodes = pd.read_csv('NodesRefStop.csv', usecols=[3, 4, 5], header=0,
-                     names=['stop_id_ref', 'stop_lat_ref', 'stop_lon_ref'])
-    nodes = nodes[nodes.stop_id_ref.str[0] == 'T']
-    print nodes
+    dbf = Dbf5('Ref Layers/Estaciones/Estaciones.dbf')
+    df = dbf.to_dataframe()
+    df.to_csv('estaciones.csv', columns=['NOMBRE', 'UBICACION', 'TRONCAL', 'ID', 'latitude', 'longitude'], encoding='utf-8', index=False)
 
-    dbf = Dbf5('Ref Layers/Estaciones/Estaciones/Estaciones.dbf')
-    dbf = dbf.to_dataframe()
-    dbf.to_csv('estaciones.csv', columns=['ID', 'latitude', 'longitude'], index=False)
-    global ref_stops
-    ref_stops = pd.read_csv('estaciones.csv', header=0, names=['stop_id_ref', 'stop_lat_ref', 'stop_lon_ref'],
-                            index_col=False)
-    ref_stops['stop_type_ref'] = 'T'
+    global stations
+    stations = pd.read_csv('estaciones.csv', header=0, usecols=[3, 4, 5], names=['station_id', 'station_lat', 'station_lon'], index_col=False)
+    stations['stop_type_ref'] = 'T'
 
     with closing(multiprocessing.Pool()) as pool:
         # joined_rows will contain lists of joined rows in arbitrary order.
         # use name=None so we get proper tuples, pandas named tuples cannot be pickled, see https://github.com/pandas-dev/pandas/issues/11791
-        joined_rows = pool.imap_unordered(join_rows, nodes.itertuples(name=None))
+        joined_rows = pool.imap_unordered(join_rows_parent, trunk_stops.itertuples(name=None))
 
         # open file and write out all rows from incoming lists of rows
-        with open('RefStopParent.csv', 'w') as f:
-            with open('RefStopOrphans.csv', 'w') as un:
+        with open('trunkstop_parent.csv', 'w') as f:
+            with open('trunkstop_orphans.csv', 'w') as un:
                 writer = csv.writer(f)
                 un_writer = csv.writer(un)
+                writer.writerow(columns_trunkstop_parent)
+                un_writer.writerow(columns_trunkstop_parent)
                 for row_list in joined_rows:
                     min_dist = sys.maxint
                     min_row = ''
@@ -136,20 +134,24 @@ def main():
                         if int(row[7]) < min_dist:
                             min_dist = int(row[7])
                             min_row = row
-                    if int(min_row[7]) > 100:
+                    if int(min_row[7]) > 500:
                         un_writer.writerow(min_row)
                     else:
                         writer.writerow(min_row)
 
-    with open('stops.txt', 'w') as f:
-        writer = csv.writer(f, encoding='utf-8', delimiter=',')
-        dbf.to_csv('estaciones.csv', columns=['NOMBRE', 'UBICACION', 'latitude', 'longitude'], encoding='utf-8')
-        estaciones = pd.read_csv('estaciones.csv', header=0, names=['stop_id', 'stop_name', 'stop_desc', 'stop_lat', 'stop_lon'])
-        for row in estaciones.itertuples(name=None):
-            writer.writerow([unicode(row[1]), row[2], row[3], unicode(row[4]), unicode(row[5]), '1', ' '])
+    # with open('stops.txt', 'w') as f:
+    #     writer = csv.writer(f, encoding='utf-8', delimiter=',')
+    #     stations = pd.read_csv('estaciones.csv', header=0, names=['stop_id', 'stop_name', 'stop_desc', 'stop_lat', 'stop_lon'], index_col=False)
+    #     for row in stations.itertuples(name=None):
+    #         writer.writerow([unicode(row[1]), row[2], row[3], unicode(row[4]), unicode(row[5]), '1', ' '])
 
+    # print counts of points
+    print 'Nodos de paraderos: ' + str(len(nodes))
+    print 'Nodos de estaciones BRT: ' + str(len(trunk_stops))
+    print 'Cenefas: ' + str(len(ref_stops))
+    print 'Estaciones BRT: ' + str(len(stations))
 
-def join_rows(row):
+def join_rows_unify(row):
     # for row in nodes.itertuples():
     l = []
     for row2 in ref_stops.itertuples(name=None):
@@ -157,6 +159,13 @@ def join_rows(row):
                   row2[2], row2[3], row2[4], haversine(row[3], row[2], row2[3], row2[2])))
     return l
 
+def join_rows_parent(row):
+    # for row in nodes.itertuples():
+    l = []
+    for row2 in stations.itertuples(name=None):
+        l.append((row[1], row[2], row[3], int(row2[1]),
+                  row2[2], row2[3], row2[4], haversine(row[3], row[2], row2[3], row2[2])))
+    return l
 
 def haversine(lon1, lat1, lon2, lat2):
     """
@@ -171,9 +180,10 @@ def haversine(lon1, lat1, lon2, lat2):
     dlat = lat2 - lat1
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a))
-    r = 6378137 # Radius of earth in meters. Use 3956 for miles
+    r = 6378137 # Equatorial radius of earth in meters.
     return c * r
 
-
+time_start = time.time()
 if __name__ == '__main__':
     main()
+print 'Nodos unificados en ' + str(common.seconds_readable(int(time.time() - time_start)))    
